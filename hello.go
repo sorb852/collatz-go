a03f35be836a5d91f8dc011cd49f4d6880497179
sorb852 <reeldob34@gmail.com>
2026-07-07 11:30:12 +0800

    idk maybe i was wrong with go
    this aint that bad

--- hello.go
+++ hello.go
@@ -1,19 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"path/filepath"
+)
 
-func add(a float32, b float32) float32 {
-	return a + b
+func apply_rule(x int) int {
+	if x % 2 == 0 {
+		return x / 2
+	} else {
+		return 3 * x + 1
+	}
+}
+
+func create_chain(n int) []int {
+	var out = []int{}
+	for i := n; true; i = apply_rule(i) {
+		out = append(out, i)
+		if i == 1 { break }
+	}
+	return out
 }
 
 func main() {
-	var a float32
-	var b float32
-	fmt.Print("Please input 'a' and 'b': ")
-	fmt.Scan(&a, &b)
-	out := add(a, b)
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "No argument given")
+		os.Exit(1)
+	}
+	if args[0] == "-h" || args[0] == "--help" {
+		fmt.Fprintf(os.Stderr, "Usage: %v [-h | --help | N]\n", filepath.Base(os.Args[0]))
+		os.Exit(0)
+	}
 
-	fmt.Printf("'a' is type %T and value %v\n", a, a)
-	fmt.Printf("'b' is type %T and value %v\n", b, b)
-	fmt.Printf("'add(a + b)' is type %T and value %v\n", out, out)
+	var n_str string = args[0]
+
+	n, err := strconv.Atoi(n_str)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Please input a valid string")
+		os.Exit(1)
+	}
+
+	if n < 1 {
+		fmt.Fprintln(os.Stderr, "N must be a natural number")
+		os.Exit(1)
+	}
+
+	chain := create_chain(n)
+	fmt.Println("Created chain:")
+	fmt.Printf("[%v", chain[0])
+	for i := 1; i < len(chain); i++ {
+		fmt.Printf(", %v", chain[i])
+	}
+	fmt.Println("]")
+
+	fmt.Printf("Element count: %v\n", len(chain))
+	fmt.Printf("Peak of graph: %v\n", slices.Max(chain))
 }
